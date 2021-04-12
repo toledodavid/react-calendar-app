946563870f14c0fc8da5a0518868b01b37161b73
David Toledo <dtoledoz0207@gmail.com>
2021-04-12 11:59:59 -0500

    create-first-components-for-auth-calendar-and-ui-also-install-react-router-dom-and-create-routes-in-AppRouter

---
+++ src/CalendarApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppRouter from './router/AppRouter';
+
+
+
+const CalendarApp = () => {
+  return(
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+
+export default CalendarApp;
---
+++ src/components/auth/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const LoginScreen = () => {
+  return(
+    <div>
+      <h1>LoginScreen</h1>
+    </div>
+  );
+}
+
+
+
+export default LoginScreen;
