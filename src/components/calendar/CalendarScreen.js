c9745c138468d3a497e85fbadce75bbb07703a0f
David Toledo <dtoledoz0207@gmail.com>
2021-04-13 11:27:13 -0500

    install-react-big-calendar-and-moment-and-use-it-in-CalendarScreen-component

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import Navbar from '../ui/Navbar';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
 
+import Navbar from '../ui/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+  }
+];
 
 
 const CalendarScreen = () => {
   return(
-    <div>
+    <div className="calendar-screen">
       <Navbar />
-      <h1>CalendarScreen</h1>
+      
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   );
 }
