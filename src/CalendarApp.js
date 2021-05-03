7989bc5d35bdd3e2a2c79fba8632bc5a2590eea2
David Toledo <dtoledoz0207@gmail.com>
2021-05-03 13:05:35 -0500

    install-redux-react-redux-and-redux-thunk-also-create-types-reducers-store-and-actions-folders

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
 import AppRouter from './router/AppRouter';
+
 
 
 
 const CalendarApp = () => {
   return(
-    <div>
+    <Provider store={store}>
       <AppRouter />
-    </div>
+    </Provider>
   );
 }
 
